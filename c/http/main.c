6cbaa09b5ad1f6af9e0ef6804f128df3e3d8c8ad
Takafumi Hirata <takhirata6@gmail.com>
2016-03-13 16:17:45 +0900

    Parse argv as url

--- c/http/main.c
+++ c/http/main.c
@@ -23,19 +23,52 @@ int main(int argc, char const* argv[])
     const int BUFLEN = 1024;
     char buffer[BUFLEN];
     char body[2048];
-    if (argc < 3) {
-        fprintf(stderr, "usage %s hostname port\n", argv[0]);
+
+    char scheme[8];
+    memset(scheme, 0, 8);
+    char hostname[256];
+    memset(hostname, 0, 256);
+    char path[256];
+    memset(path, 0, 256);
+
+    if (argc < 1) {
+        fprintf(stderr, "usage %s hostname\n", argv[0]);
         exit(1);
     }
 
-    portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
 
     printf("sock open: %d\n", sockfd);
 
-    server = gethostbyname(argv[1]);
+    // parse argv[1]
+    int ret = sscanf(argv[1],
+                   "%8[^\n:]://%[^\n/?]%[^\n]",
+                   scheme,
+                   hostname,
+                   path);
+
+    if (ret < 3) {
+        memset(scheme, 0, 8);
+        strcpy(scheme, "http");
+        memset(hostname, 0, 256);
+        memset(path, 0, 256);
+        ret = sscanf(argv[1], "%[^\n/?]%[^\n]", hostname, path);
+    }
+    ret = sscanf(hostname, "%[^\n:/?]:%d", hostname, &portno);
+    if (path[0] == 0) {
+        path[0] = '/';
+    }
+    if (portno == 0) {
+        portno = 80;
+    }
+    printf("scheme: %s\n", scheme);
+    printf("hostname: %s\n", hostname);
+    printf("path: %s\n", path);
+    printf("portno: %d\n", portno);
+
+    server = gethostbyname(hostname);
     if (server == NULL) {
         fprintf(stderr, "ERROR, no such host\n");
         exit(0);
@@ -52,10 +85,8 @@ int main(int argc, char const* argv[])
     }
     printf("connect\n");
     memset(buffer, 0, BUFLEN);
-    const char* host = argv[1];
-    printf("%s(length %lu)\n", host, strlen(host));
     char message[100];
-    sprintf(message, "GET / HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", host);
+    sprintf(message, "GET / HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", hostname);
     printf("message = %s\n", message);
     n = write(sockfd, message, strlen(message));
     printf("write %d bytes\n", n);
