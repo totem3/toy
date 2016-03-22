1201da23d981f2ec3a33a62ac64cd3778c41fc7d
Takafumi Hirata <takhirata6@gmail.com>
2016-03-22 19:56:57 +0900

    add print function

--- c/dict/dict.c
+++ c/dict/dict.c
@@ -7,7 +7,7 @@ void f(tree_t *t)
     t->value = t->value * 2;
 }
 
-int main(int argc, char const* argv[])
+int main()
 {
     dict_entry_t entry;
     entry.key = "test";
@@ -23,21 +23,28 @@ int main(int argc, char const* argv[])
     printf("created first tree\n");
     printf("t.value: %d\n", t.value);
     printf("create second tree\n");
-    tree_t *t2;
-    t2->left = NULL;
-    t2->right = NULL;
-    t2->value = 200;
-    printf("t2->value: %d\n", t2->value);
-    insert_node(&t, t2);
+    tree_t t2;
+    t2.left = NULL;
+    t2.right = NULL;
+    t2.value = 200;
+    printf("t2->value: %d\n", t2.value);
     tree_t t3 = {
         .left = NULL,
         .right = NULL,
         .value = 50
     };
+    tree_t t4 = {
+        .left = NULL,
+        .right = NULL,
+        .value = 300
+    };
     printf("t3->value: %d\n", t3.value);
     insert_node(&t, &t3);
-    print_tree(&t);
+    insert_node(&t3, &t2);
+    insert_node(&t, &t4);
     traverse(&t, f);
-    print_tree(&t);
+    printf("t2 height: %d\n", tree_height(&t2));
+    printf("t1 height: %d\n", tree_height(&t));
+    print_tree(&t, 0);
     return 0;
 }
