051c294848afaf394b7e4f17ebac1874de29811f
Takafumi Hirata <takhirata6@gmail.com>
2016-04-08 07:17:32 +0900

    impl hash map
    とても雑に実行してみた。速いしメモリもこっちのほうが使わない

--- c/hash/fnv.c
+++ c/hash/fnv.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define OFFSET_BASIS 2166136261
 #define FNV_PRIME 16777619
+#define TABLE_SIZE 1000000
+
+/* int collision_count = 0; */
 unsigned int fnv_hash(const char *key)
 {
     unsigned int hash = OFFSET_BASIS;
@@ -12,17 +16,54 @@ unsigned int fnv_hash(const char *key)
     return hash;
 }
 
-void usage(const char *program)
+typedef struct entry {
+    const char *key;
+    int value;
+} Entry;
+
+typedef struct hash_table {
+    Entry *entries;
+} Map;
+
+Map *new_map()
 {
-    printf("usage: %s <int>\n", program);
+    Entry *entries =calloc(sizeof(Entry), TABLE_SIZE);
+    Map *map = calloc(sizeof(Map), 1);
+    map->entries = entries;
+    return map;
+}
+
+void set(Map *m, const char *key, int value)
+{
+    int index = fnv_hash(key) % TABLE_SIZE;
+    /* if (m->entries[index].key != NULL) { */
+    /*     collision_count++; */
+    /*     printf("key collision: %s & %s\n", key, m->entries[index].key); */
+    /* } */
+    m->entries[index].key = key;
+    m->entries[index].value = value;
+}
+
+Entry *get(Map *m, const char *key)
+{
+    int index = fnv_hash(key) % TABLE_SIZE;
+    return &m->entries[index];
 }
 
 int main(int argc, char const* argv[])
 {
-
-    const char *keys[3] = {"foo", "bar", "baz"};
-    for (int i = 0; i < 3; i++) {
-        printf("%s => %u\n", keys[i], fnv_hash(keys[i]));
+    Map *map = new_map();
+    int TIMES = 1000000;
+    char keys[TIMES][8];
+    memset(keys, 0, sizeof(keys));
+    for (int i = 0; i < TIMES; i++) {
+        sprintf(keys[i], "%d", i);
+        set(map, keys[i], i);
     }
+    for (int i = 0; i < TIMES; i++) {
+        Entry *e = get(map, keys[i]);
+        /* printf("%s => %d\n", e->key, e->value); */
+    }
+
     return 0;
 }
