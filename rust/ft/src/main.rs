7520b2837e826c8ee2b953d32b7770de3e3fb1bf
Takafumi Hirata <takhirata6@gmail.com>
2018-01-01 17:52:05 +0900

    Use ofuton crate

--- rust/ft/src/main.rs
+++ rust/ft/src/main.rs
@@ -1,6 +1,7 @@
 extern crate rustfft;
 extern crate ndarray;
 extern crate image;
+extern crate ofuton;
 
 use rustfft::{FFTplanner, FFTnum};
 use rustfft::num_complex::Complex;
@@ -8,71 +9,6 @@ use rustfft::num_traits::Zero;
 use ndarray::{Array, Array2, ArrayView, ArrayViewMut, ArrayViewMut2, Dimension};
 use image::{ColorType, GenericImage, DynamicImage};
 use std::fmt::Debug;
-
-fn _fft<T: FFTnum>(input: &mut [Complex<T>], output: &mut [Complex<T>], inverse: bool) {
-    let mut planner = FFTplanner::new(inverse);
-    let len = input.len();
-    let fft = planner.plan_fft(len);
-    fft.process(input, output);
-}
-
-fn fft<T: FFTnum>(input: &mut [Complex<T>], output: &mut [Complex<T>]) {
-    _fft(input, output, false);
-}
-
-fn ifft<T: FFTnum + From<u32>>(input: &mut [Complex<T>], output: &mut [Complex<T>]) {
-    _fft(input, output, true);
-    for v in output.iter_mut() {
-        *v = v.unscale(T::from(input.len() as u32));
-    }
-}
-
-fn fft2(input: &mut ArrayViewMut2<Complex<f64>>, output: &mut ArrayViewMut2<Complex<f64>>) {
-    fftnd(input, output, &[0,1]);
-}
-
-fn ifft2(input: &mut ArrayViewMut2<Complex<f64>>, output: &mut ArrayViewMut2<Complex<f64>>) {
-    ifftnd(input, output, &[1,0]);
-}
-
-fn fftn<D: Dimension>(input: &mut ArrayViewMut<Complex<f64>, D>, output: &mut ArrayViewMut<Complex<f64>, D>, axis: usize) {
-    _fftn(input, output, axis, false);
-}
-
-fn ifftn<D: Dimension>(input: &mut ArrayViewMut<Complex<f64>, D>, output: &mut ArrayViewMut<Complex<f64>, D>, axis: usize) {
-    _fftn(input, output, axis, true);
-}
-
-fn _fftn<D: Dimension>(input: &mut ArrayViewMut<Complex<f64>, D>, output: &mut ArrayViewMut<Complex<f64>, D>, axis: usize, inverse: bool) {
-    if inverse {
-        mutate_lane(input, output, ifft, axis)
-    } else {
-        mutate_lane(input, output, fft, axis)
-    }
-}
-
-fn fftnd<D: Dimension>(input: &mut ArrayViewMut<Complex<f64>, D>, output: &mut ArrayViewMut<Complex<f64>, D>, axes: &[usize]) {
-    _fftnd(input, output, axes, false);
-}
-
-fn ifftnd<D: Dimension>(input: &mut ArrayViewMut<Complex<f64>, D>, output: &mut ArrayViewMut<Complex<f64>, D>, axes: &[usize]) {
-    _fftnd(input, output, axes, true);
-}
-
-fn _fftnd<D: Dimension>(input: &mut ArrayViewMut<Complex<f64>, D>, output: &mut ArrayViewMut<Complex<f64>, D>, axes: &[usize], inverse: bool) {
-    let len = axes.len();
-    for i in 0..len {
-        let axis = axes[i];
-        _fftn(input, output, axis, inverse);
-        if i < len - 1 {
-            let mut outrows = output.genrows_mut().into_iter();
-            for mut row in input.genrows_mut() {
-                let mut outrow = outrows.next().unwrap();
-                row.as_slice_mut().unwrap().copy_from_slice(outrow.as_slice_mut().unwrap());
-            }
-        }
-    }
-}
 
 fn shift<T:Default+ Copy+ Clone>(view: &ArrayViewMut2<T>) -> Vec<T> {
     let dimensions = view.raw_dim().into_pattern();
@@ -117,37 +53,10 @@ fn spectral(img: &DynamicImage) -> Vec<Complex<f64>> {
     let mut outputbuffer = vec![Zero::zero(); buffer.len()];
     let mut input = ArrayViewMut::from_shape(shape, &mut buffer).unwrap();
     let mut output = ArrayViewMut::from_shape(shape, &mut outputbuffer).unwrap();
-    fft2(&mut input, &mut output);
+    ofuton::fft2(&mut input, &mut output);
     shift(&output)
     // let shifted = shift(&output);
     // normalize(shifted)
-}
-
-fn mutate_lane<T: Zero + Clone + Debug, D: Dimension>(input: &mut ArrayViewMut<T, D>, output: &mut ArrayViewMut<T, D>, f: fn(&mut [T], &mut [T]) -> (), axis: usize) {
-    if axis > 0 {
-        input.swap_axes(0, axis);
-        output.swap_axes(0, axis);
-        {
-            let mut outrows = output.genrows_mut().into_iter();
-            for mut row in input.genrows_mut() {
-                let mut outrow = outrows.next().unwrap();
-                let mut vec = row.to_vec();
-                let mut out = vec![Zero::zero(); outrow.len()];
-                f(&mut vec, &mut out);
-                for i in 0..outrow.len() {
-                    outrow[i] = out.remove(0);
-                }
-            }
-        }
-        input.swap_axes(0, axis);
-        output.swap_axes(0, axis);
-    } else {
-        let mut outrows = output.genrows_mut().into_iter();
-        for mut row in input.genrows_mut() {
-            let mut outrow = outrows.next().unwrap();
-            f(&mut row.as_slice_mut().unwrap(), &mut outrow.as_slice_mut().unwrap());
-        }
-    }
 }
 
 fn low_pass_filter_inplace(mut input: &mut ArrayViewMut2<Complex<f64>>, r: usize) {
@@ -225,14 +134,14 @@ fn main() {
     let mut output = ArrayViewMut::from_shape(shape, &mut outputbuffer).unwrap();
     {
         let mut result = ArrayViewMut::from_shape(shape, &mut resultbuffer).unwrap();
-        fft2(&mut input, &mut output);
+        ofuton::fft2(&mut input, &mut output);
         let mut shifted_buffer = shift(&output);
         let mut shifted = ArrayViewMut::from_shape(shape, &mut shifted_buffer).unwrap();
         low_pass_filter_inplace(&mut shifted, filter_size);
         let shifted = shifted.view_mut();
         let mut unshifted_buffer = shift(&shifted);
         let mut unshifted = ArrayViewMut::from_shape(shape, &mut unshifted_buffer).unwrap();
-        ifft2(&mut unshifted, &mut result);
+        ofuton::ifft2(&mut unshifted, &mut result);
     }
     let result: Vec<u8> = resultbuffer.into_iter().map(|x| x.re as u8).collect();
     let filename = format!("high_pass_filter_{}.jpg", filter_size);
@@ -263,7 +172,7 @@ fn _main() {
     let mut result_vec = vec![Zero::zero(); image_array.shape()[0] * image_array.shape()[1]];
     {
         let mut result = ArrayViewMut::from_shape((image_array.shape()[0], image_array.shape()[1]), &mut result_vec).unwrap();
-        ifft2(&mut image_array, &mut result);
+        ofuton::ifft2(&mut image_array, &mut result);
     }
     let fimage: Vec<u8> = result_vec.into_iter().map(|x| x.re as u8).collect();
     println!("{:?}", &fimage);
@@ -444,8 +353,8 @@ mod test {
         let a:Vec<Complex<f64>> = vec![1.,2.,3.,4.,5.,6.,7.,8.,9.].into_iter().map(|i|Complex::from(i)).collect();
         let mut b = vec![Zero::zero(); 9];
         let mut b2 = vec![Zero::zero(); 9];
-        fft(&mut a.clone(), &mut b);
-        ifft(&mut b, &mut b2);
+        ofuton::fft(&mut a.clone(), &mut b);
+        ofuton::ifft(&mut b, &mut b2);
         for (a, b) in a.iter().zip(b2) {
             assert!((a.re-b.re).abs() < 0.0001)
         }
@@ -460,8 +369,8 @@ mod test {
         let mut b2 = vec![Zero::zero(); 9];
         {
             let mut output2 = ArrayViewMut::from_shape((3,3), &mut b2).unwrap();
-            fft2(&mut input, &mut output);
-            ifft2(&mut output, &mut output2);
+            ofuton::fft2(&mut input, &mut output);
+            ofuton::ifft2(&mut output, &mut output2);
         }
         let a:Vec<Complex<f64>> = vec![1.,2.,3.,4.,5.,6.,7.,8.,9.].into_iter().map(|i|Complex::from(i)).collect();
         for (a, b) in a.iter().zip(b2) {
